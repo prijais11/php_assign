890166759e01ba742ab7332356e559d7aba56c7d
prijais11 <priyajaiswal@cedcoss.com>
2022-02-14 19:43:26 +0530

    Task 8 done

--- src/work.php
+++ src/work.php
@@ -1,10 +1,24 @@
+<table width="400px" height="30px" cellspacing="0px" cellpadding="0px" border="1px">
 <?php
-$n=6;$fact=1;
-for ($x = 1; $x <= $n; $x++) {
-    $fact=$fact*$x ;
+for($row=1;$row<=8;$row++)
+{
+	echo "<tr>";
+	for($column=1;$column<=8;$column++)
+	{
+		$total=$row+$column;
+		if($total%2==0)
+		{
+			echo "<td height=35px width=30px bgcolor=#FFFFFF></td>";
+		}
+		else
+		{
+			echo "<td height=35px width=30px bgcolor=#000000></td>";
+		}
+	}
+	echo "</tr>";
 }
-    echo "factorial of ".$n." is : ".$fact;
 ?>
+</table>
 <!DOCTYPE html>
 <html lang="en">
 <head>
