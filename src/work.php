5927957f1f025d8ab11df0d116739f114c05ebc2
prijais11 <priyajaiswal@cedcoss.com>
2022-02-14 19:10:16 +0530

    task 5 done

--- src/work.php
+++ src/work.php
@@ -1,7 +1,14 @@
 <?php
-  $color = "red";
-  $status="new";
-  echo "My car is " . $color." and ".$status.".";
+$marks = 10;
+if ($marks > "60") {
+  echo "First Division";
+} elseif ($marks > "45" && $marks <"59") {
+  echo "Second Division";
+} elseif ($marks > "33" && $marks <"44") {
+    echo "Third Division";
+} else {
+  echo "Fail";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
