158ec9d5989166962fdd7703c53e6103c93f4240
prijais11 <priyajaiswal@cedcoss.com>
2022-02-14 18:45:03 +0530

    Task1 done

---
+++ src/work.php
@@ -0,0 +1,18 @@
+<?php
+for ($x = 5; $x <= 15; $x++) {
+  echo "The count is: $x <br>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="work.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="work.js"></script>
+</body>
+</html>
