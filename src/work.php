7da5c02c4c90452ab70fe1c4d25172c198985cf3
prijais11 <priyajaiswal@cedcoss.com>
2022-02-14 19:19:56 +0530

    Task 6 done

--- src/work.php
+++ src/work.php
@@ -1,13 +1,29 @@
 <?php
-$marks = 10;
-if ($marks > "60") {
-  echo "First Division";
-} elseif ($marks > "45" && $marks <"59") {
-  echo "Second Division";
-} elseif ($marks > "33" && $marks <"44") {
-    echo "Third Division";
-} else {
-  echo "Fail";
+$days= 5;
+switch ($days) {
+case 1:
+    echo "Monday";
+    break;
+case 2:
+    echo "Tuesday";
+    break;
+case 3:
+    echo "Wednesday";
+    break;
+case 4:
+    echo "Thursday";
+    break;
+case 5:
+    echo "Friday";
+    break;
+case 6:
+    echo "Saturday";
+    break;
+case 7:
+    echo "Sunday";
+    break;
+default:
+    echo "invalid input";
 }
 ?>
 <!DOCTYPE html>
